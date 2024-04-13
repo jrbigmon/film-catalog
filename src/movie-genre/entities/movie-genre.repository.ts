3de5ca786720641e3567d29da496073bbf01acf8
VagnerJr <juniorterriaga0@gmail.com>
2024-04-13 16:25:59 -0300

    feat: init movie genre repository implementation

---
+++ src/movie-genre/entities/movie-genre.repository.ts
@@ -0,0 +1,21 @@
+import { IFindAllFilters } from '../../utils/interfaces/find-all-filters.interface';
+import { MovieGenre } from './movie-genre.entity';
+import { IMovieGenreRepository } from './movie-genre.repository.interface';
+
+export class MovieGenreRepository implements IMovieGenreRepository {
+  findById(id: string): Promise<MovieGenre> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(filters?: IFindAllFilters<MovieGenre>): Promise<MovieGenre[]> {
+    throw new Error('Method not implemented.');
+  }
+  create(movieGenre: MovieGenre): Promise<MovieGenre> {
+    throw new Error('Method not implemented.');
+  }
+  update(id: string, movieGenre: MovieGenre): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
