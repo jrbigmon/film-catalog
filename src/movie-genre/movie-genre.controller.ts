2af5cc4f20cee61bd1328112025275ed55f489fb
VagnerJr <juniorterriaga0@gmail.com>
2024-04-13 19:16:33 -0300

    feat: return json response

--- src/movie-genre/movie-genre.controller.ts
+++ src/movie-genre/movie-genre.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   Res,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { MovieGenreService } from './movie-genre.service';
 import { CreateMovieGenreDto } from './dto/create-movie-genre.dto';
@@ -19,52 +21,65 @@ export class MovieGenreController {
   constructor(private readonly movieGenreService: MovieGenreService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async create(
     @Body() createMovieGenreDto: CreateMovieGenreDto,
     @Res() res: Response,
   ) {
     try {
-      return await this.movieGenreService.create(createMovieGenreDto);
+      const result = await this.movieGenreService.create(createMovieGenreDto);
+      return res.json(result);
     } catch (error) {
       return ExceptionsControllers.getException(error, res);
     }
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   async findAll(@Res() res: Response) {
     try {
-      return await this.movieGenreService.findAll();
+      const result = await this.movieGenreService.findAll();
+      return res.json(result);
     } catch (error) {
       return ExceptionsControllers.getException(error, res);
     }
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   async findOne(@Param('id') id: string, @Res() res: Response) {
     try {
-      return await this.movieGenreService.findOne(id);
+      const result = await this.movieGenreService.findOne(id);
+      return res.json(result);
     } catch (error) {
       return ExceptionsControllers.getException(error, res);
     }
   }
 
   @Patch(':id')
+  @HttpCode(HttpStatus.OK)
   async update(
     @Param('id') id: string,
     @Body() updateMovieGenreDto: UpdateMovieGenreDto,
     @Res() res: Response,
   ) {
     try {
-      return await this.movieGenreService.update(id, updateMovieGenreDto);
+      const result = await this.movieGenreService.update(
+        id,
+        updateMovieGenreDto,
+      );
+      return res.json(result);
     } catch (error) {
       return ExceptionsControllers.getException(error, res);
     }
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: string, @Res() res: Response) {
     try {
-      return await this.movieGenreService.remove(id);
+      const result = await this.movieGenreService.remove(id);
+      return res.json(result);
     } catch (error) {
       return ExceptionsControllers.getException(error, res);
     }
