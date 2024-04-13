6246622b819357b83f8dabad71b17ab92dc2fcaf
VagnerJr <juniorterriaga0@gmail.com>
2024-04-13 19:07:44 -0300

    feat: register repository in memory

--- src/movie-genre/movie-genre.module.ts
+++ src/movie-genre/movie-genre.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MovieGenreService } from './movie-genre.service';
 import { MovieGenreController } from './movie-genre.controller';
+import { MovieGenreRepositoryInMemory } from './entities/movie-genre.repository.in.memory';
 
 @Module({
   controllers: [MovieGenreController],
-  providers: [MovieGenreService],
+  providers: [
+    MovieGenreService,
+    {
+      provide: 'MovieGenreRepository',
+      useValue: new MovieGenreRepositoryInMemory(),
+    },
+  ],
 })
 export class MovieGenreModule {}
