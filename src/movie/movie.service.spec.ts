2300a72e65f09a6ff8521d23aa3cdc28182effaa
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 00:44:21 -0300

    test: create a new movie with movie genres

--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { MovieGenre } from '../movie-genre/entities/movie-genre.entity';
+import { MovieGenreService } from '../movie-genre/movie-genre.service';
+import { IMovieGenreService } from '../movie-genre/movie-genre.service.interface';
+import { MovieGenreRepositoryInMemory } from '../movie-genre/repository/movie-genre.repository.in.memory';
+import { IMovieGenreRepository } from '../movie-genre/repository/movie-genre.repository.interface';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { MovieService } from './movie.service';
+import { MovieRepositoryInMemory } from './repository/movie.repository.in.memory';
+import { IMovieRepository } from './repository/movie.repository.interface';
 
 describe('MovieService', () => {
   let service: MovieService;
+  let repository: IMovieRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MovieService],
-    }).compile();
+  let movieGenreService: IMovieGenreService;
+  let movieGenreRepository: IMovieGenreRepository;
 
-    service = module.get<MovieService>(MovieService);
+  beforeAll(() => {
+    movieGenreRepository = new MovieGenreRepositoryInMemory([
+      new MovieGenre('Action', '123', new Date(), new Date()),
+      new MovieGenre('Drama', '123', new Date(), new Date()),
+      new MovieGenre('Suspense', '123', new Date(), new Date()),
+    ]);
+    movieGenreService = new MovieGenreService(movieGenreRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Create', () => {
+    beforeEach(() => {
+      repository = new MovieRepositoryInMemory();
+      service = new MovieService(repository, movieGenreService);
+    });
+
+    it('should be create a new movie', async () => {
+      const payload: CreateMovieDto = {
+        title: 'The best movie ever',
+        cast: ['me', 'me2'],
+        director: 'me',
+        country: 'Greenland',
+        durationMinutes: 300,
+        genres: ['Action', 'Drama'],
+        language: 'Latin',
+        rating: 10,
+        releaseYear: 2030,
+        synopsis: 'The best movie ever myself',
+      };
+
+      const movieCreated = await service.create(payload);
+
+      expect(movieCreated).not.toBeNull();
+      expect(movieCreated.getId()).not.toBeNull();
+      expect(movieCreated.getGenres()).toHaveLength(2);
+    });
   });
 });
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { IMovieRepository } from './repository/movie.repository.interface';
 import { IMovieGenreService } from '../movie-genre/movie-genre.service.interface';
+import { ExceptionsServices } from '../utils/exceptions/exceptions-services';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MovieService {
@@ -11,8 +13,29 @@ export class MovieService {
     private readonly movieGenreService: IMovieGenreService,
   ) {}
 
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+  async create(createMovieDto: CreateMovieDto) {
+    const { genres, ...rest } = createMovieDto;
+
+    const genresInDB = await this.movieGenreService.findAll({
+      filters: { name: genres },
+    });
+
+    if (!genresInDB?.length) {
+      throw new ExceptionsServices(
+        `Movie Genres not found`,
+        HttpStatus.BAD_REQUEST,
+        'genres',
+      );
+    }
+
+    const newMovie = Movie.create({
+      ...rest,
+      genres: genresInDB,
+    });
+
+    const movieCreated = await this.repository.create(newMovie);
+
+    return movieCreated;
   }
 
   findAll() {
