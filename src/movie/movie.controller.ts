bc7df7f4414777cff2a169daf8ff90a127f630f6
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 01:30:31 -0300

    feat: responses adjusted

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,34 +1,80 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { ExceptionsControllers } from '../utils/exceptions/expcetions-controllers';
+import { Response } from 'express';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Post()
-  create(@Body() createMovieDto: CreateMovieDto) {
-    return this.movieService.create(createMovieDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createMovieDto: CreateMovieDto, @Res() res: Response) {
+    try {
+      const result = await this.movieService.create(createMovieDto);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Get()
-  findAll() {
-    return this.movieService.findAll();
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Res() res: Response) {
+    try {
+      const result = await this.movieService.findAll();
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.movieService.findOne(+id);
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    try {
+      return await this.movieService.findOne(id);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
-    return this.movieService.update(+id, updateMovieDto);
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id') id: string,
+    @Body() updateMovieDto: UpdateMovieDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const result = await this.movieService.update(id, updateMovieDto);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.movieService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const result = await this.movieService.remove(+id);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 }
