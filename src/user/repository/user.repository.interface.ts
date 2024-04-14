9042809606feb8b21d230411971dcf3bbb16857d
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 02:44:22 -0300

    feat: create a new user

--- src/user/repository/user.repository.interface.ts
+++ src/user/repository/user.repository.interface.ts
@@ -1,8 +1,10 @@
 import { IFindAllFilters } from '../../utils/interfaces/find-all-filters.interface';
+import { IFindByFilter } from '../../utils/interfaces/find-by-filters.interface';
 import { User } from '../entities/user.entity';
 
 export interface IUserRepository {
   findById(id: string): Promise<User>;
+  findByFilter(filter: IFindByFilter): Promise<User>;
   findAll(filters: IFindAllFilters): Promise<User[]>;
   create(user: User): Promise<User>;
   update(id: string, user: User): Promise<boolean>;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { IUserRepository } from './repository/user.repository.interface';
+import { ExceptionsServices } from '../utils/exceptions/exceptions-services';
+import { Crypto } from '../utils/functions/crypto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly repository: IUserRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, password, username, email } = createUserDto;
+
+    const getUsernameExistent = this.repository.findByFilter({
+      username,
+    });
+
+    const getEmailExistent = this.repository.findByFilter({ email });
+
+    const [usernameExists, emailExists] = await Promise.all([
+      getUsernameExistent,
+      getEmailExistent,
+    ]);
+
+    if (usernameExists) {
+      throw new ExceptionsServices(
+        'Username already exists',
+        HttpStatus.BAD_REQUEST,
+        'username',
+      );
+    }
+
+    if (emailExists) {
+      throw new ExceptionsServices(
+        'Email already exists',
+        HttpStatus.BAD_REQUEST,
+        'email',
+      );
+    }
+
+    const passwordEncrypted = Crypto.encrypt(password);
+
+    const newUser = new User(name, email, username, passwordEncrypted);
+
+    const userCreated = await this.repository.create(newUser);
+
+    return userCreated;
   }
 
   findAll() {
