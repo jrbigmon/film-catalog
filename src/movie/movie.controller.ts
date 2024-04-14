e0383628c0f246b0fa9c777788d5daedffdc6fb9
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 13:31:00 -0300

    feat: user guards movie routes

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -9,18 +9,21 @@ import {
   HttpCode,
   HttpStatus,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { ExceptionsControllers } from '../utils/exceptions/exceptions-controllers';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() createMovieDto: CreateMovieDto, @Res() res: Response) {
     try {
@@ -32,6 +35,7 @@ export class MovieController {
   }
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   async findAll(@Res() res: Response) {
     try {
@@ -43,6 +47,7 @@ export class MovieController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   async findOne(@Param('id') id: string, @Res() res: Response) {
     try {
@@ -54,6 +59,7 @@ export class MovieController {
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.OK)
   async update(
     @Param('id') id: string,
@@ -69,6 +75,7 @@ export class MovieController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: string, @Res() res: Response) {
     try {
