56dacd817f004ff9eb854c18bafd8a81159c4bfa
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 02:08:52 -0300

    feat: init user endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovieGenreModule } from './movie-genre/movie-genre.module';
 import { MovieModule } from './movie/movie.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MovieGenreModule, MovieModule],
+  imports: [MovieGenreModule, MovieModule, UserModule],
   controllers: [],
   providers: [],
 })
