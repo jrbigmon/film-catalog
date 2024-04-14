a54251c15560b56bf3c62e0aa2219008c54bc8d9
VagnerJr <juniorterriaga0@gmail.com>
2024-04-13 23:37:36 -0300

    feat: movie create dto

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,1 +1,13 @@
-export class CreateMovieDto {}
+export class CreateMovieDto {
+  title: string;
+  genres: string[];
+  director: string;
+  cast: string[];
+  releaseYear: number;
+  durationMinutes: number;
+  rating: number;
+  synopsis: string;
+  language: string;
+  country: string;
+  posterURL?: string;
+}
