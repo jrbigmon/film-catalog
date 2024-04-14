96680e8a90f732cb298636ae1792a281c854fcaa
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 12:18:32 -0300

    feat: implements response json and http status code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,58 @@
-import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
+import { ExceptionsControllers } from '../utils/exceptions/expcetions-controllers';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    try {
+      const result = await this.userService.create(createUserDto);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const result = this.userService.update(id, updateUserDto);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const result = this.userService.remove(id);
+      return res.json(result);
+    } catch (error) {
+      return ExceptionsControllers.getException(error, res);
+    }
   }
 }
