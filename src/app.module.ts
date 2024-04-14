6e1dc7450ee368838aa476731f40d121ece3de59
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 17:49:59 -0300

    feat: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { MovieModule } from './movie/movie.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -12,6 +14,17 @@ import { ConfigModule } from '@nestjs/config';
     MovieModule,
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.BD_HOST || 'localhost',
+      port: Number(process.env.BD_PORT) || 3306,
+      username: process.env.BD_USER || 'root',
+      password: process.env.DB_PASS || 'root',
+      database: process.env.DB_NAME || 'test',
+      entities: [],
+    }),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
