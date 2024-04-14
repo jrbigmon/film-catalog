522ef75db3373ea00a7ea10fbc9b5080de55b3e6
VagnerJr <juniorterriaga0@gmail.com>
2024-04-14 12:47:04 -0300

    chore: config module to get env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { MovieGenreModule } from './movie-genre/movie-genre.module';
 import { MovieModule } from './movie/movie.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MovieGenreModule, MovieModule, UserModule],
+  imports: [
+    AuthModule,
+    MovieGenreModule,
+    MovieModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
