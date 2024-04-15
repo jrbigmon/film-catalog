057aadcebeab440445d5bd06ddc377115cbcc886
jrbigmon <juniorterriaga0@gmail.com>
2024-04-14 22:28:00 -0300

    feat: creating movie repository

--- src/movie/repository/movie.repository.ts
+++ src/movie/repository/movie.repository.ts
@@ -14,21 +14,70 @@ export class MovieRepository implements IMovieRepository {
     private readonly movieRepositoryTypeOrm: Repository<MovieRepositoryTypeOrm>,
   ) {}
 
-  findById(id: string): Promise<Movie> {
-    throw new Error('Method not implemented.');
+  private movieMount(movie: MovieRepositoryTypeOrm): Movie {
+    const {
+      id,
+      title,
+      genres,
+      director,
+      cast,
+      releaseYear,
+      durationMinutes,
+      rating,
+      language,
+      country,
+      synopsis,
+      posterURL,
+      createdAt,
+      updatedAt,
+      deletedAt,
+    } = movie;
+
+    return new Movie(
+      title,
+      genres,
+      director,
+      cast,
+      releaseYear,
+      durationMinutes,
+      rating,
+      language,
+      country,
+      synopsis,
+      posterURL,
+      id,
+      createdAt,
+      updatedAt,
+      deletedAt,
+    );
   }
-  findByFilter(filter: IFindByFilter): Promise<Movie> {
-    throw new Error('Method not implemented.');
+
+  async findById(id: string): Promise<Movie> {
+    const movie = await this.movieRepositoryTypeOrm.findOneBy({ id });
+
+    return this.movieMount(movie);
   }
-  findAll(filters?: IFindAllFilters): Promise<Movie[]> {
-    throw new Error('Method not implemented.');
+  async findByFilter(filter: IFindByFilter): Promise<Movie> {
+    const movie = await this.movieRepositoryTypeOrm.findOneBy(filter);
+    return this.movieMount(movie);
   }
-  create(movie: Movie): Promise<Movie> {
-    throw new Error('Method not implemented.');
+
+  async findAll(filters?: IFindAllFilters): Promise<Movie[]> {
+    const movies = await this.movieRepositoryTypeOrm.find(filters.filters);
+
+    if (!movies?.length) return [];
+
+    return movies?.map((movie) => this.movieMount(movie));
   }
+
+  async create(movie: Movie): Promise<Movie> {
+    const movieCreated = await this.movieRepositoryTypeOrm.save({ ...movie });
+  }
+
   update(id: string, movie: Movie): Promise<boolean> {
     throw new Error('Method not implemented.');
   }
+
   delete(id: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
