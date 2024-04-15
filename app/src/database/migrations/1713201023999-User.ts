5b843c3499448c153c052529d928387162947de4
VagnerJr <juniorterriaga0@gmail.com>
2024-04-15 19:36:31 -0300

    feat: changed migration to query runner interface

--- app/src/database/migrations/1713201023999-User.ts
+++ app/src/database/migrations/1713201023999-User.ts
@@ -1,26 +1,58 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class User1713201023999 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const sql = `
-        CREATE TABLE IF NOT EXISTS user (
-            id VARCHAR(255) NOT NULL PRIMARY KEY,
-            name VARCHAR(150) NOT NULL,
-            email VARCHAR(200) NOT NULL,
-            username VARCHAR(100) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP NOT NULL,
-            updated_at TIMESTAMP NOT NULL,
-            deleted_at TIMESTAMP,
-        );
-    `;
-
-    await queryRunner.query(sql);
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar(255)',
+            isUnique: true,
+            isPrimary: true,
+            isNullable: false,
+          },
+          {
+            name: 'name',
+            type: 'varchar(100)',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar(200)',
+            isNullable: false,
+          },
+          {
+            name: 'username',
+            type: 'varchar(100)',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar(255)',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const sql = `DROP TABLE user;`;
-
-    await queryRunner.query(sql);
+    await queryRunner.dropTable('user');
   }
 }
