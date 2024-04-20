1bdc91cb48e02b393c821a86bb9da56743b08619
VagnerJr <juniorterriaga0@gmail.com>
2024-04-20 20:33:22 -0300

    feat: try get production connection, if error, catch default connection

--- app/src/database/ormconfig.js
+++ app/src/database/ormconfig.js
@@ -3,8 +3,25 @@ const { readFileSync } = require('fs');
 const { resolve } = require('path');
 const { DataSource } = require('typeorm');
 
-const databaseConfig = readFileSync(
-  resolve('src', 'database', 'database.config.json'),
-);
+let databaseConfig = '';
+
+try {
+  databaseConfig = readFileSync(
+    resolve('src', 'database', 'database.config.json'),
+  );
+} catch (_) {
+  try {
+    console.error(
+      'Config to production database not found, using the sample file',
+    );
+
+    databaseConfig = readFileSync(
+      resolve('src', 'database', 'database.config.sample.json'),
+    );
+  } catch (error) {
+    console.error('Production and sample database config not found', err);
+    throw error;
+  }
+}
 
 module.exports = new DataSource(JSON.parse(databaseConfig));
